4fc62a2351b5299827f40204ad0e289af2707029
Marco Marche <marco.marche@gmail.com>
2023-04-21 09:07:24 +0200

    build: add support for mbed patches

--- docker/build.sh
+++ docker/build.sh
@@ -9,8 +9,11 @@ set -e
 BASE_DIR=`pwd`
 API_DIR="${BASE_DIR}/cores/arduino"
 PATCHES_DIR="/patches"
+ARDUINO_PATCHES_DIR="${PATCHES_DIR}/arduino"
+MBED_PATCHES_DIR="${PATCHES_DIR}/mbed"
 BUILD_VARIANTS=("NICLA" "ARDUINO_NANO33BLE" "PORTENTA_H7_M7")
 CORE_MBED_VERSION="4.0.2"
+MBED_OS_DIR="/tmp/mbed-os-program/mbed-os"
 
 curl -sSL "https://github.com/arduino/ArduinoCore-mbed/tarball/${CORE_MBED_HASH}" | tar --strip-components 1 -x -z
 curl -sSL "https://github.com/arduino/ArduinoCore-API/tarball/${API_HASH}" | tar --strip-components 1 -x -z -C "${API_DIR}" "arduino-ArduinoCore-API-${API_HASH}/api"
@@ -21,10 +24,17 @@ set +e
 ./mbed-os-to-arduino -b "${MBED_OS_VERSION}" -a NOPE:NOPE
 set -e
 
-for p in `ls ${PATCHES_DIR}`
+for p in `ls ${ARDUINO_PATCHES_DIR}`
 do
-  patch -p1 -i "${PATCHES_DIR}/${p}"
+  patch -p1 -i "${ARDUINO_PATCHES_DIR}/${p}"
 done
+
+cd "${MBED_OS_DIR}"
+for p in `ls ${MBED_PATCHES_DIR}`
+do
+  patch -p1 -i "${MBED_PATCHES_DIR}/${p}"
+done
+cd "${BASE_DIR}"
 
 for v in "${BUILD_VARIANTS[@]}"
 do
